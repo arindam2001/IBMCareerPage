818f86aa247c66c315237a00881ce307db522f11
Arindam Sal <arindam.sal.edu@gmail.com>
2023-07-21 15:29:21 +0530

    First Commit

---
+++ src/TestComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestComponent = () => {
+  return (
+    <div className="text-red-500">
+      <h2>This is a Test Component</h2>
+      <p>This paragraph should have red text color.</p>
+    </div>
+  );
+};
+
+export default TestComponent;
